8e380057591dd60ae7de8fa1e99c6fa8014416af
ZachThomas324234 <146828835+ZachThomas324234@users.noreply.github.com>
2024-10-15 16:13:17 +0100

    Tuesday 15/10/24
    -terrain
    -footsteps
    peak

---
+++ Assets/Scripts/player/PlayerFootsteps.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    public AudioClip[] footsteps;
+
+    public float timeBetweenSteps, stepTimer;
+
+    public AudioManager am;
+
+    public PlayerMovement pm;
+
+    public bool IsMoving()
+    {
+        if (pm.MovementX != 0 || pm.MovementY != 0) return true;
+        else return false;
+    }
+
+    public void Update()
+    {
+        if (IsMoving()) stepTimer -= Time.deltaTime;
+        if (stepTimer <= 0)
+        {
+            stepTimer = timeBetweenSteps;
+            am.PlayRandomSound(footsteps, 1, 1, 0.1f);
+        }
+    }
+}
